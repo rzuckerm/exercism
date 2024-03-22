83b6e6972b25cd4d53622e17ddfffdd75a3b8abe
Ron Zuckerman <ron.zuckerman@adtran.com>
2024-03-22 08:22:17 -0500

    Simplify Spiral Matrix in Rust

--- rust/spiral-matrix/src/lib.rs
+++ rust/spiral-matrix/src/lib.rs
@@ -1,19 +1,16 @@
 pub fn spiral_matrix(size: u32) -> Vec<Vec<u32>> {
-    let mut position: (i32, i32) = (0, 0);
-    let mut rotation: (i32, i32) = (0, 1);
-    let mut count: (i32, i32) = (size as i32, size as i32);
-    let mut offset: (i32, i32) = (0, 1);
+    let mut position = (0, 0);
+    let mut direction = (0, 1);
     let mut matrix: Vec<Vec<u32>> = vec![vec![0; size as usize]; size as usize];
-    for i in 1..=(size * size) {
-        matrix[position.0 as usize][position.1 as usize] = i;
-        count = (count.0 - offset.0, count.1 - offset.1);
-        if count.0 == 0 || count.1 == 0 {
-            rotation = (rotation.1, -rotation.0);
-            offset = (offset.1, offset.0);
-            count = (count.0 + count.1 - offset.0, count.0 + count.1 - offset.0);
+    let mut n = 1u32..;
+    for turns_left in (0..2 * size as i32).rev() {
+        for _ in 0..(turns_left + 1) / 2 {
+            matrix[position.0 as usize][position.1 as usize] = n.next().unwrap();
+            position = (position.0 + direction.0, position.1 + direction.1);
         }
-
-        position = (position.0 + rotation.0, position.1 + rotation.1);
+        position.0 += direction.1 - direction.0;
+        position.1 -= direction.1 + direction.0;
+        direction = (direction.1, -direction.0);
     }
 
     matrix
