d0aa1a2d026a86287563ed9356387a7c414f044e
rzuckerm <ron.zuckerman@adtran.com>
2024-04-25 18:06:53 -0500

    Improve Dominoes in Rust

--- rust/dominoes/src/lib.rs
+++ rust/dominoes/src/lib.rs
@@ -1,20 +1,14 @@
-use std::collections::BTreeMap;
-
-type ChainKey = (usize, bool);
-
 pub fn chain(input: &[(u8, u8)]) -> Option<Vec<(u8, u8)>> {
     match input.len() {
         0 => Some(vec![]),
         1 => (input[0].0 == input[0].1).then_some(vec![input[0]]),
         _ => {
             let chainables = get_chainables(input)?;
-            for &key in chainables.keys() {
+            for key in 0..2 * input.len() {
                 let mut visited = vec![false; input.len()];
                 let mut output = Vec::<(u8, u8)>::new();
-                if let Some(chain) =
-                    find_first_chain_for_key(key, input, &chainables, &mut visited, &mut output)
-                {
-                    return Some(chain);
+                if find_first_chain_for_key(key, input, &chainables, &mut visited, &mut output) {
+                    return Some(output);
                 }
             }
 
@@ -23,21 +17,17 @@ pub fn chain(input: &[(u8, u8)]) -> Option<Vec<(u8, u8)>> {
     }
 }
 
-fn get_chainables(input: &[(u8, u8)]) -> Option<BTreeMap<ChainKey, Vec<ChainKey>>> {
-    let mut chainables = BTreeMap::<ChainKey, Vec<ChainKey>>::new();
+fn get_chainables(input: &[(u8, u8)]) -> Option<Vec<Vec<usize>>> {
+    let mut chainables = vec![vec![]; 2 * input.len()];
     for (i, &(first1, second1)) in input.iter().enumerate() {
-        for (second, forward) in [(second1, true), (first1, false)] {
+        for (second, index) in [(second1, 2 * i), (first1, 2 * i + 1)] {
             for (j, &(first2, second2)) in input.iter().enumerate() {
-                if i == j {
-                    continue;
-                }
-
-                if second == first2 || second == second2 {
-                    (*chainables.entry((i, forward)).or_default()).push((j, second == first2));
+                if i != j && (second == first2 || second == second2) {
+                    chainables[index].push(2 * j + (second == second2) as usize);
                 }
             }
 
-            chainables.get(&(i, forward))?;
+            chainables[index].first()?;
         }
     }
 
@@ -45,34 +35,31 @@ fn get_chainables(input: &[(u8, u8)]) -> Option<BTreeMap<ChainKey, Vec<ChainKey>
 }
 
 fn find_first_chain_for_key(
-    key: ChainKey,
+    key: usize,
     input: &[(u8, u8)],
-    chainables: &BTreeMap<ChainKey, Vec<ChainKey>>,
+    chainables: &Vec<Vec<usize>>,
     visited: &mut Vec<bool>,
     output: &mut Vec<(u8, u8)>,
-) -> Option<Vec<(u8, u8)>> {
-    visited[key.0] = true;
-    match key.1 {
-        true => output.push(input[key.0]),
-        false => output.push((input[key.0].1, input[key.0].0)),
+) -> bool {
+    visited[key / 2] = true;
+    match key % 2 {
+        0 => output.push(input[key / 2]),
+        _ => output.push((input[key / 2].1, input[key / 2].0)),
     }
 
     if output.len() == input.len() {
-        return (output[0].0 == output.last().unwrap().1).then_some(output.clone());
+        return output[0].0 == output.last().unwrap().1;
     }
 
-    for &next_key in &chainables[&key] {
-        if !visited[next_key.0] {
-            if let Some(next_output) =
-                find_first_chain_for_key(next_key, input, chainables, visited, output)
-            {
-                return Some(next_output);
-            }
+    for next_key in chainables[key].clone() {
+        if !visited[next_key / 2]
+            && find_first_chain_for_key(next_key, input, chainables, visited, output)
+        {
+            return true;
         }
     }
 
-    visited[key.0] = false;
+    visited[key / 2] = false;
     output.pop();
-
-    None
+    false
 }
