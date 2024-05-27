038c82fe560c9891fc4351b85a9145c963781796
rzuckerm <ron.zuckerman@adtran.com>
2024-05-26 20:10:27 -0500

    Use single loop for Ledger in Python

--- python/ledger/ledger.py
+++ python/ledger/ledger.py
@@ -6,7 +6,7 @@ class LedgerEntry:
     def __init__(self, date: str, description: str, change: int):
         self.date = datetime.strptime(date, "%Y-%m-%d")
         self.description = description
-        self.change = change
+        self.change = change / 100
 
     def __eq__(self, other) -> bool:
         return (self.date, self.change, self.description) == (other.date, other.change, other.description)
@@ -20,63 +20,47 @@ def create_entry(date: str, description: str, change: int) -> LedgerEntry:
 
 
 class LedgerEntryFormatter:
-    def __init__(self, date_hdr: str, descr_hdr: str, change_hdr: str):
-        self.date_hdr = date_hdr
-        self.descr_hdr = descr_hdr
-        self.change_hdr = change_hdr
+    def __init__(self, currency: str, locale: str):
+        self.curr_symbol = "$" if currency == "USD" else "€"
+        if locale == "en_US":
+            self.date_hdr, self.descr_hdr, self.change_hdr = "Date", "Description", "Change"
+            self.date_format = "%m/%d/%Y"
+            self.change_func = self.change_en
+        else:
+            self.date_hdr, self.descr_hdr, self.change_hdr = "Datum", "Omschrijving", "Verandering"
+            self.date_format = "%d-%m-%Y"
+            self.change_func = self.change_nl
 
     def header(self) -> str:
         return f"{self.date_hdr:10} | {self.descr_hdr:25} | {self.change_hdr:13}"
 
+    def date_(self, date: datetime) -> str:
+        return date.strftime(self.date_format)
+
     def description(self, description: str) -> str:
         return f"{description[:22]}..." if len(description) > 25 else description.ljust(25)
 
-    def change_en_us(self, curr_symbol: str, change: int) -> str:
-        change = change / 100
-        return (f"({curr_symbol}{-change:,.2f})" if change < 0 else f"{curr_symbol}{change:,.2f} ").rjust(13)
+    def change_en(self, change: int) -> str:
+        return (f"({self.curr_symbol}{-change:,.2f})" if change < 0 else f"{self.curr_symbol}{change:,.2f} ").rjust(13)
 
-    def change_nl_nl(self, curr_symbol: str, change: int) -> str:
-        change = change / 100
-        return f"{curr_symbol} {change:,.2f} ".replace(",", ":").replace(".", ",").replace(":", ".").rjust(13)
+    def change_nl(self, change: int) -> str:
+        return f"{self.curr_symbol} {change:,.2f} ".replace(",", ":").replace(".", ",").replace(":", ".").rjust(13)
 
 
 def format_entries(currency: str, locale: str, entries: list[LedgerEntry]) -> str:
-    table = ""
-    if locale == "en_US":
-        formatter = LedgerEntryFormatter("Date", "Description", "Change")
+    # Generate Header Row
+    formatter = LedgerEntryFormatter(currency, locale)
+    table = [formatter.header()]
+    for entry in sorted(entries):
+        # Write entry date to table
+        line = formatter.date_(entry.date) + " | "
 
-        # Generate Header Row
-        table += formatter.header()
+        # Write entry description to table
+        # Truncate if necessary
+        line += formatter.description(entry.description) + " | "
 
-        for entry in sorted(entries):
-            table += "\n"
+        # Write entry change to table
+        line += formatter.change_func(entry.change)
+        table.append(line)
 
-            # Write entry date to table
-            table += entry.date.strftime("%m/%d/%Y") + " | "
-
-            # Write entry description to table
-            # Truncate if necessary
-            table += formatter.description(entry.description) + " | "
-
-            # Write entry change to table
-            table += formatter.change_en_us("$" if currency == "USD" else "€", entry.change)
-    elif locale == "nl_NL":
-        formatter = LedgerEntryFormatter("Datum", "Omschrijving", "Verandering")
-
-        # Generate Header Row
-        table += formatter.header()
-
-        for entry in sorted(entries):
-            table += "\n"
-
-            # Write entry date to table
-            table += entry.date.strftime("%d-%m-%Y") + " | "
-
-            # Write entry description to table
-            # Truncate if necessary
-            table += formatter.description(entry.description) + " | "
-
-            # Write entry change to table
-            table += formatter.change_nl_nl("$" if currency == "USD" else "€", entry.change)
-
-    return table
+    return "\n".join(table)
