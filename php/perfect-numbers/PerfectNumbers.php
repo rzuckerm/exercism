686cccf4d4c18e6b252017826ec29620d4e565b3
rzuckerm <ron.zuckerman@adtran.com>
2024-06-24 18:50:32 -0500

    Add Perfect Numbers in PHP

---
+++ php/perfect-numbers/PerfectNumbers.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+function getClassification(int $number): string
+{
+    if ($number < 1) {
+        throw new \InvalidArgumentException("Classification is only possible for positive integers.");
+    }
+
+    $q = (int) sqrt($number);
+    $factors = [];
+    for ($i = 1; $i <= $q; $i++) {
+        if ($number % $i == 0) {
+            array_push($factors, $i, (int)($number / $i));
+        }
+    }
+
+    $s = -$number + array_sum(array_unique($factors));
+    return ["perfect", "deficient", "abundant"][(int) ($s < $number) + 2 * (int) ($s > $number)];
+}
