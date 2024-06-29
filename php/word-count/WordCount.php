294a5563e823547b7d7ff809f2294cd83d26be84
rzuckerm <ron.zuckerman@adtran.com>
2024-06-28 21:17:02 -0500

    Fix Word Count in PHP

--- php/word-count/WordCount.php
+++ php/word-count/WordCount.php
@@ -4,5 +4,6 @@ declare(strict_types=1);
 
 function wordCount(string $words): array
 {
-    return array_count_values(str_word_count(strtolower($words), 1, "0123456789"));
+    preg_match_all('/[a-z0-9]+(?:\'[a-z0-9]+)?/', strtolower($words), $matches);
+    return array_count_values(array_map(null, ...$matches));
 }
