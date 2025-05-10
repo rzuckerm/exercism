4cf11b864dc6a79516a5879deab08572457941ae
rzuckerm <ron.zuckerman@adtran.com>
2025-05-10 14:23:56 -0500

    Add ETL in PowerShell

---
+++ powershell/etl/Etl.ps1
@@ -0,0 +1,23 @@
+Function Invoke-Etl() {
+    <#
+    .SYNOPSIS
+    Transforms a set of legacy Lexiconia data stored as letters per score to a set of data stored score per letter.
+
+    .DESCRIPTION
+    Take a hash table and take the values as keys and the keys as values.
+
+    .PARAMETER Legacy
+    The legacy data to transform.
+
+    .EXAMPLE
+    Invoke-Etl -Legacy @{1 = @("A")}
+    #>
+    [CmdletBinding()]
+    Param(
+        [object]$Legacy
+    )
+
+    $Result = @{}
+    $Legacy.Keys | ForEach-Object { $Score = $_; $Legacy[$Score] | ForEach-Object { $Result[$_.ToLower()] = $Score } }
+    $Result
+}
