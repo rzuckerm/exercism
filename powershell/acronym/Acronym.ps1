1acbb5e611b0819079ec0ff6deb8da8186e2b9be
rzuckerm <ron.zuckerman@adtran.com>
2025-05-11 18:22:37 -0500

    Add Acronym in PowerShell

---
+++ powershell/acronym/Acronym.ps1
@@ -0,0 +1,21 @@
+Function Get-Acronym() {
+    <#
+    .SYNOPSIS
+    Get the acronym of a phrase.
+
+    .DESCRIPTION
+    Given a phrase, return the string acronym of that phrase.
+    "As Soon As Possible" => "ASAP"
+    
+    .PARAMETER Phrase
+    The phrase to get the acronym from.
+    
+    .EXAMPLE
+    Get-Acronym -Phrase "As Soon As Possible"
+    #>
+    [CmdletBinding()]
+    Param (
+        [string]$Phrase
+    )
+    ($Phrase -creplace "([A-Za-z])[A-Za-z']+", "`$1" -creplace "[^A-Za-z]", "").ToUpper()
+}
