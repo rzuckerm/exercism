43cfa5c1cd8cce84a4e75f810956047f71a83d3e
rzuckerm <ron.zuckerman@adtran.com>
2025-05-16 20:04:56 -0500

    Add Word Count in PowerShell

---
+++ powershell/word-count/WordCount.ps1
@@ -0,0 +1,23 @@
+Function Get-WordCount() {
+    <#
+    .SYNOPSIS
+    Given a phrase, count how many time each word appear.
+
+    .DESCRIPTION
+    Count how many time each word appear in a phrase. Number in string also counted as word, and words are case insensitive.
+
+    .PARAMETER Phrase
+    The phrase to count words.
+
+    .EXAMPLE
+    Get-WordCount -Phrase "Hello, welcome to exercism!"
+    Return: @{ hello = 1; welcome = 1; to = 1; exercism = 1}
+    #>
+    [CmdletBinding()]
+    Param(
+        [string]$Phrase
+    )
+    $Count = @{}
+    ($Phrase.ToLower() | Select-String -AllMatches "[a-z\d]+(?:'[a-z\d]+)?").matches.Value | ForEach-Object { $Count[$_] += 1 }
+    $Count
+}
