8a0751c7b2bab020a42cc3ebb75a018e822d1183
rzuckerm <ron.zuckerman@adtran.com>
2025-05-17 08:21:16 -0500

    Add Matching Brackets in PowerShell

---
+++ powershell/matching-brackets/MatchingBrackets.ps1
@@ -0,0 +1,25 @@
+Function Test-MatchingBrackets() {
+    <#
+    .SYNOPSIS
+    Determine if all brackets inside a string paired and nested correctly.
+    
+    .DESCRIPTION
+    Given a string containing brackets `[]`, braces `{}`, parentheses `()`, or any combination thereof, verify that any and all pairs are matched and nested correctly.
+    The string may also contain other characters, which for the purposes of this exercise should be ignored.
+    
+    .PARAMETER Text
+    The string to be verified.
+    
+    .EXAMPLE
+    Test-MatchingBrackets("[]") => True
+    Test-MatchingBrackets("[)]") => False
+    #>
+    [CmdletBinding()]
+    Param(
+        [string]$Text
+    )
+
+    $Brackets = $Text -replace "[^(){}\[\]]", ""
+    while ($Brackets -match "\(\)|\{\}|\[\]") { $Brackets = $Brackets -replace "\(\)|\{\}|\[\]", "" }
+    -not $Brackets
+}
