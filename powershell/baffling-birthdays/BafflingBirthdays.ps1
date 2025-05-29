66afa40b071ae10d89e4dd1a04cd3d9c61afa562
rzuckerm <ron.zuckerman@adtran.com>
2025-05-28 19:55:59 -0500

    Add Baffling Birthdays in Powershell

---
+++ powershell/baffling-birthdays/BafflingBirthdays.ps1
@@ -0,0 +1,55 @@
+Function Invoke-BafflingBirthdays {
+    <#
+    .SYNOPSIS
+    Estimate the birthday paradox's probabilities.
+
+    .DESCRIPTION
+    To solve the birthday paradox exercise you need to do these steps:
+    - Generate random birthdates.
+    - Check if a collection of randomly generated birthdates contains at least two with the same birthday.
+    - Estimate the probability that at least two people in a group share the same birthday for different group sizes.
+
+    .PARAMETER People
+    Number of people in the group.
+    #>
+    [CmdletBinding()]
+    Param(
+        [int]$People
+    )
+
+    100 * (1..1000 | ForEach-Object { Test-SharedBirthday (Get-RandomBirthdates $People) } | Measure-Object -Average).Average
+}
+
+Function Get-RandomBirthdates {
+    <#
+    .SYNOPSIS
+    Generate a collection of birthdates based on a number of people.
+
+    .PARAMETER People
+    Number of birthdates to be generated.
+    #>
+    [CmdletBinding()]
+    Param(
+        [int]$People
+    )
+
+    do { $year = Get-Random -Minimum 1925 -Maximum 2026 } while ([DateTime]::IsLeapYear($year))
+    Get-Random -Minimum 0 -Maximum 365 -Count $People | ForEach-Object { [DateTime]::new($year, 1, 1).AddDays($_) }
+}
+
+Function Test-SharedBirthday {
+    <#
+    .SYNOPSIS
+    Check if there is a shared birthday in a list of random birthdates.
+
+    .PARAMETER Birthdates
+    A collection of random Datetime objects represents.
+    #>
+    [CmdletBinding()]
+    Param(
+        [DateTime[]]$Birthdates
+    )
+
+    $BirthDates.Length -gt 1 -and
+        ($BirthDates | Group-Object -Property Month, Day | Measure-Object).Count -ne $BirthDates.Length
+}
