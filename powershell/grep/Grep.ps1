5e58dde6b6d469b4848ce9ea1f97c31a5047cbdd
rzuckerm <ron.zuckerman@adtran.com>
2025-06-08 17:04:57 -0500

    Add Grep in PowerShell

---
+++ powershell/grep/Grep.ps1
@@ -0,0 +1,85 @@
+Function Invoke-Grep() {
+    <#
+    .SYNOPSIS
+    Implement a grep function similar to the grep command in unix system.
+
+    .DESCRIPTION
+    The Unix grep command searches files for lines that match a regular expression.
+    Your task is to implement a simplified grep command, which supports searching for fixed strings.
+
+    The grep command takes three arguments:
+
+    1. The string to search for.
+    2. Zero or more flags for customizing the command's behavior.
+    3. One or more files to search in.
+
+    It then reads the contents of the specified files (in the order specified), finds the lines that contain the search string, and finally returns those lines in the order in which they were found.
+    When searching in multiple files, each matching line is prepended by the file name and a colon (':').
+    Attempt to read file that doesn't exist should throw an error.
+
+    .PARAMETER Pattern
+    The string to be matched
+
+    .PARAMETER Files
+    An array of string, each represent a text file to be accessed and get to its content
+
+    .PARAMETER Number
+    A switch parameter, similar to '-n' flag
+
+    .PARAMETER List
+    A switch parameter, similar to '-l' flag
+
+    .PARAMETER Insensitive
+    A switch parameter, similar to '-i' flag
+
+    .PARAMETER Invert
+    A switch parameter, similar to '-v' flag
+
+    .PARAMETER Whole
+    A switch parameter, similar to '-x' flag
+
+    .EXAMPLE
+    ```greeting.txt
+    @"
+    Hello everybody,
+    Welcome to Exercism!
+    Please enjoy the Powershell track,
+    and have fun coding!
+    "@
+    ```
+    Grepping the word 'power' as case-insensitive in file "greeting.txt", also show line number:
+
+    Invoke-Grep -Pattern "power" -Files @("greeting.txt") -Number -Insensitive
+    Returns: @("3:Please enjoy the Powershell track,")
+    #>
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$True)]
+        [string]$Pattern,
+        [Parameter(Mandatory=$True,ValuefromPipeline=$True)]
+        [string[]]$Files,
+        [switch]$Number,
+        [switch]$List,
+        [switch]$Insensitive,
+        [switch]$Invert,
+        [switch]$Whole
+    )
+
+    $matchPattern = ($Insensitive) ? $Pattern.ToLower() : $Pattern
+    foreach ($file in $Files) {
+        $lineNum = 0
+        foreach ($line in (Get-Content -Path $file)) {
+            $lineNum++
+            $matchLine = ($Insensitive) ? $line.ToLower() : $line
+            $match = ($Whole) ? ($matchLine -ceq $matchPattern) : ($matchLine -cmatch $matchPattern)
+            if ($match -xor $Invert) {
+                if ($List) {
+                    Write-Output $file
+                    break
+                }
+
+                Write-Output ((($Files.Count -gt 1) ? "${file}:" : "") + (($Number) ? "${lineNum}:" : "") + $line)
+            }
+        }
+    }
+}
