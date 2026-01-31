098d8015d582ef51e55c8668f42661f03912ef0a
rzuckerm <rzdulc@gmail.com>
2026-01-31 08:10:08 -0600

    Remove one more line from Camicia in Python

--- python/camicia/camicia.py
+++ python/camicia/camicia.py
@@ -5,7 +5,7 @@ def simulate_game(player_a, player_b):
     debts = tuple(deque("JQKA".find(card) + 1 for card in hand) for hand in (player_a, player_b))
     prior_states, pile, current_player, debt, cards_played, tricks, status = set(), [], 0, 0, 0, 0, "loop"
 
-    while True:
+    while status != "finished":
         current_state = tuple(tuple(debt) for debt in debts)
         if current_state in prior_states:
             break
@@ -33,6 +33,5 @@ def simulate_game(player_a, player_b):
         tricks += 1
         if not pile and (not debts[0] or not debts[1]):
             status = "finished"
-            break
 
     return {"status": status, "tricks": tricks, "cards": cards_played}
