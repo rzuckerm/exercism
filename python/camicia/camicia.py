3f0799ee8f9afb6182f495c8acc9b9d2a5a29f87
rzuckerm <rzdulc@gmail.com>
2026-01-31 06:53:14 -0600

    Improve Camicia in Python

--- python/camicia/camicia.py
+++ python/camicia/camicia.py
@@ -2,63 +2,37 @@ from collections import deque
 
 
 def simulate_game(player_a, player_b):
-    status = "loop"
-    game_state = GameState(player_a, player_b)
-    prior_states = set()
+    debts = tuple(deque("JQKA".find(card) + 1 for card in hand) for hand in (player_a, player_b))
+    prior_states, pile, current_player, debt, cards_played, tricks, status = set(), [], 0, 0, 0, 0, "loop"
 
     while True:
-        current_state = game_state.get_state()
+        current_state = tuple(tuple(debt) for debt in debts)
         if current_state in prior_states:
             break
 
-        if any(card in "JQKA" for hand in (player_a, player_b) for card in hand):
+        if any(any(debt) for debt in debts):
             prior_states.add(current_state)
 
-        if game_state.play_turn_and_check_if_done():
+        while True:
+            orig_debt = debt
+            if not (current_debt := debts[current_player]):
+                break
+
+            pile.append(new_debt := current_debt.popleft())
+            cards_played += 1
+
+            if (debt := new_debt or max(debt - 1, 0)) == 0 and orig_debt > 0:
+                break
+
+            if new_debt > 0 or debt == 0:
+                current_player = 1 - current_player
+
+        current_player = 1 - current_player
+        debts[current_player].extend(pile)
+        pile = []
+        tricks += 1
+        if not pile and (not debts[0] or not debts[1]):
             status = "finished"
             break
 
-    return {"status": status, "tricks": game_state.tricks, "cards": game_state.cards_played}
-
-
-class GameState:
-    def __init__(self, player_a, player_b):
-        self.hands = (deque(player_a), deque(player_b))
-        self.pile = []
-        self.current_player, self.debt, self.cards_played, self.tricks = 0, 0, 0, 0
-
-    def get_state(self):
-        return tuple(" ".join(card if card in "JQKA" else "-" for card in hand) for hand in self.hands)
-
-    def play_turn_and_check_if_done(self):
-        while True:
-            orig_debt = self.debt
-            current_hand = self.hands[self.current_player]
-            if not current_hand:
-                self.transfer_pile()
-                break
-
-            self.pile.append(top_card := current_hand.popleft())
-            self.cards_played += 1
-
-            if (new_debt := "JQKA".find(top_card) + 1) > 0:
-                self.debt = new_debt
-            else:
-                self.debt = max(self.debt - 1, 0)
-                if orig_debt > 0 and self.debt == 0:
-                    self.transfer_pile()
-                    self.next_player()
-                    break
-
-            if new_debt > 0 or self.debt == 0:
-                self.next_player()
-
-        return not self.pile and (not self.hands[0] or not self.hands[1])
-
-    def next_player(self):
-        self.current_player = 1 - self.current_player
-
-    def transfer_pile(self):
-        self.hands[1 - self.current_player].extend(self.pile)
-        self.pile.clear()
-        self.tricks += 1
+    return {"status": status, "tricks": tricks, "cards": cards_played}
